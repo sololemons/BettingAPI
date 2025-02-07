27b86c6d4c908b0c08918a60fcbc9ad3dca3168e
SolomonDev <solohlemons75@gmail.com>
2025-02-07 18:31:01 +0300

    second commit

--- BettingApi/src/main/java/com/BettingApi/BETTING/REPOSITORIES/betSlipRepository.java
+++ BettingApi/src/main/java/com/BettingApi/BETTING/REPOSITORIES/betSlipRepository.java
@@ -1,4 +1,5 @@
 package com.BettingApi.BETTING.REPOSITORIES;
+import com.BettingApi.BETTING.ENTITIES.Bet;
 import com.BettingApi.BETTING.ENTITIES.BetSlip;
 import com.BettingApi.BETTING.ENTITIES.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import java.util.List;
 @Repository
 public interface betSlipRepository extends JpaRepository<BetSlip, Long> {
     List<BetSlip> findByBet_Users(Users users);
+
+    List<BetSlip> findByBet(Bet bet);
 }
